0d3f4d1cc16f5dc879078dc6a1ecb3dd5ffaf3db
한재경 <hjg000223@naver.com>
2025-01-15 15:38:01 +0900

    feat: BoardResponse 생성자 -> 빌더패턴 적용으로 변경 - 많은 매개변수를 받는 생성자를 @Builder를 이용해 좀 더 가독성있도록 변경

--- a406_backend/src/main/java/com/social/a406/domain/board/dto/BoardResponse.java
+++ a406_backend/src/main/java/com/social/a406/domain/board/dto/BoardResponse.java
@@ -1,14 +1,12 @@
 package com.social.a406.domain.board.dto;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder
 public class BoardResponse {
     private Long boardId;
     private String content;
