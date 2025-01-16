cf920ca322b3b78f5667f8305ad59fc834e1676b
한재경 <hjg000223@naver.com>
2025-01-16 14:13:11 +0900

    feat: 게시글 좋아요 기능 추가 - 이미 좋아요 한 게시글이면 IllegalStateException 예외 발생

---
+++ a406_backend/src/main/java/com/social/a406/domain/like/controller/BoardLikeController.java
@@ -0,0 +1,23 @@
+package com.social.a406.domain.like.controller;
+
+import com.social.a406.domain.like.dto.LikedUserResponse;
+import com.social.a406.domain.like.service.BoardLikeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/boards/{boardId}/likes")
+@RequiredArgsConstructor
+public class BoardLikeController {
+
+    private final BoardLikeService boardLikeService;
+
+    @PostMapping
+    public void likeBoard(@PathVariable Long boardId, @AuthenticationPrincipal UserDetails userDetails) {
+        boardLikeService.addLike(userDetails.getUsername(), boardId);
+    }
+}
---
+++ a406_backend/src/main/java/com/social/a406/domain/like/repository/BoardLikeRepository.java
@@ -0,0 +1,13 @@
+package com.social.a406.domain.like.repository;
+
+import com.social.a406.domain.board.entity.Board;
+import com.social.a406.domain.like.entity.BoardLike;
+import com.social.a406.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
+    boolean existsByUserAndBoard(User user, Board board);
+}
