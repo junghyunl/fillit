dd1ef4270f222bd507f4cc21e79e771b98b6b319
이정현 <fnsk1256@naver.com>
2025-01-16 17:57:00 +0900

    feat: add tailwind config

---
+++ fillit_frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // JSX/TSX 파일을 포함
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
