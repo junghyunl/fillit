d78631067bd676d220ab7c5cfceec27b4a9ad0b7
한재경 <hjg000223@naver.com>
2025-01-16 14:18:21 +0900

    feat: CommentLike 엔티티 추가 - 댓글의 좋아요 기능

---
+++ a406_backend/src/main/java/com/social/a406/domain/like/entity/CommentLike.java
@@ -0,0 +1,30 @@
+package com.social.a406.domain.like.entity;
+
+import com.social.a406.domain.comment.entity.Comment;
+import com.social.a406.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class CommentLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id", nullable = false)
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public CommentLike(Comment comment, User user) {
+        this.comment = comment;
+        this.user = user;
+    }
+}
