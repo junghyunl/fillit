a78c403548ee3d3fb78ba7a5cb7d35c44c5e7d38
한재경 <hjg000223@naver.com>
2025-01-16 10:55:38 +0900

    feat: InterestRepository JPA 레포지토리 추가 - 관심사 레포지토리

---
+++ a406_backend/src/main/java/com/social/a406/domain/interest/repository/InterestRepository.java
@@ -0,0 +1,7 @@
+package com.social.a406.domain.interest.repository;
+
+import com.social.a406.domain.interest.entity.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+}
