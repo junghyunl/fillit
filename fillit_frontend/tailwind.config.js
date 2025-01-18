be66150dbb300d28305bd05daa555fbb2dad9c53
junghyunl <fnsk1256@naver.com>
2025-01-19 01:19:50 +0900

    feat: add tailwind config - tailwind.config를 ES module 형식으로 변경 - tailwind를 사용하기 위한 postcss.config 추가

--- fillit_frontend/tailwind.config.js
+++ fillit_frontend/tailwind.config.js
@@ -1,9 +1,5 @@
-// tailwind.config.js
-module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // JSX/TSX 파일을 포함
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
