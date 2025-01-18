fa2c71f562357879bc72cba7a51e183e9c185df3
junghyunl <fnsk1256@naver.com>
2025-01-19 01:17:42 +0900

    feat: update app name - app 이름을 Fillit으로 변경

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -1,9 +1,7 @@
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-blue-600">Tailwind CSS 적용 확인</h1>
-      </div>
+      <h1 className="text-blue-600">Fillit App</h1>
     </div>
   );
 };
