1f65ecb9de88642702e1df553069f36f1436a3c2
이정현 <fnsk1256@naver.com>
2025-01-23 13:41:20 +0900

    feat: 라우팅 설정 추가 - Voice, Profile, Search, Message, Login 라우팅 추가

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -1,8 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import MessagePage from './pages/MessagePage';
+import ProfilePage from './pages/ProfilePage';
+import SearchPage from './pages/SearchPage';
+import VoicePage from './pages/VoicePage';
+
 const App = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-blue-600">Fillit App</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/message" element={<MessagePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/voice" element={<VoicePage />} />
+    </Routes>
   );
 };
 
