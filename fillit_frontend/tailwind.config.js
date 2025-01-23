7a2eee4c51e39335c10bbab593cf1c1cebc02434
이정현 <fnsk1256@naver.com>
2025-01-23 10:56:46 +0900

    design: font, background-image 추가

--- fillit_frontend/tailwind.config.js
+++ fillit_frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kanit: ['Kanit', 'sans-serif'],
+      },
+      backgroundImage: {
+        'home-bg': "url('/images/bg-image.png')",
+      },
+    },
   },
   plugins: [],
 };
