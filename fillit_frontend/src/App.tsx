185cf8381aed527c108a94ead0c0bcbb358ee940
이정현 <fnsk1256@naver.com>
2025-01-23 10:56:46 +0900

    design: font, background-image 추가

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-blue-600">Fillit App</h1>
     </div>
   );
