9ffdbc379fbe2a693efcf7f1b6fcaf0938e7a9f5
이승희 <dearlsh1125@gmail.com>
2025-01-24 17:22:43 +0900

    design: 음성 페이지 배경

---
+++ fillit_frontend/src/pages/VoicePage.tsx
@@ -0,0 +1,12 @@
+import { BubbleBackground } from '../components/decorations/BubbleBackground';
+
+const VoicePage = () => {
+  return (
+    <div>
+      <h1>Voice</h1>
+      <BubbleBackground />
+    </div>
+  );
+};
+
+export default VoicePage;
