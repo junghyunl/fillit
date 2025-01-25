8a5b9d49235c4efb914e6abb2330aff49a2465b4
이정현 <fnsk1256@naver.com>
2025-01-26 08:51:14 +0900

    feat: 헤더 버튼 추가

---
+++ fillit_frontend/src/components/common/Button/MenuTabButton.tsx
@@ -0,0 +1,11 @@
+import { MenuTabIcon } from '@/assets/assets';
+
+const MenuTabButton = () => {
+  return (
+    <button>
+      <img src={MenuTabIcon} alt="menutab icon" />
+    </button>
+  );
+};
+
+export default MenuTabButton;
---
+++ fillit_frontend/src/components/common/Button/RegistButton.tsx
@@ -0,0 +1,11 @@
+import { RegistIcon } from '@/assets/assets';
+
+const RegistButton = () => {
+  return (
+    <button>
+      <img src={RegistIcon} alt="regist icon" />
+    </button>
+  );
+};
+
+export default RegistButton;
