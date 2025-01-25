7324a3ae7def5b75fe48128adaf174f7670b04ee
이정현 <fnsk1256@naver.com>
2025-01-26 08:49:15 +0900

    feat: 알림 페이지 추가

---
+++ fillit_frontend/src/pages/Notification.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/common/Header';
+
+const Notification = () => {
+  return (
+    <>
+      <Header left="back" text="Notification" isTitle={true} />
+      <h1>Notification</h1>
+    </>
+  );
+};
+
+export default Notification;
