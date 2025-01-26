e34d7f2650a419a409b76f52bdcf36f92c453b0e
한병현 <gksqudgus777@naver.com>
2025-01-26 23:33:30 +0900

    feat: 기본input 컴포넌트 추가

---
+++ fillit_frontend/src/components/common/BasicInput.tsx
@@ -0,0 +1,14 @@
+interface BasicInputProps {
+  placeholder?: string;
+}
+
+const BasicInput = ({ placeholder = 'Enter the text...' }: BasicInputProps) => {
+  return (
+    <input
+      placeholder={placeholder}
+      className="bg-[#ffffff] px-3 py-1.5 w-[280px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2]"
+    />
+  );
+};
+
+export default BasicInput;
