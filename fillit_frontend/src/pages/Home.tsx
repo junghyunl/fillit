bee77d4475add48cdd9644ef208aa7310f250bdf
이정현 <fnsk1256@naver.com>
2025-01-26 18:59:42 +0900

    fix: 이미지 크기 인라인 스타일로  변경 - tailwind 동적 스타일링 안되는 문제로 인라인 스타일로 변경

--- fillit_frontend/src/pages/Home.tsx
+++ fillit_frontend/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import Header from '@/components/common/Header';
+import ProfileImage from '@/assets/images/profile-image.png';
 
 const Home = () => {
   return (
     <>
-      <Header left="home" right="notification" />
+      <Header left="home" profileImage={ProfileImage} right="notification" />
       <h1>Home</h1>
     </>
   );
