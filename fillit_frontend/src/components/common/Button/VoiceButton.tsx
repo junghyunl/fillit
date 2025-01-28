d215b7554c61575db3094b0c1c79de7bfa713f1b
이승희 <dearlsh1125@gmail.com>
2025-01-29 03:21:29 +0900

    refactor: 폰트 색상 props에 추가

--- fillit_frontend/src/components/common/Button/VoiceButton.tsx
+++ fillit_frontend/src/components/common/Button/VoiceButton.tsx
@@ -2,17 +2,20 @@ interface VoiceButtonProps {
   text?: string;
   onClick?: () => void;
   isPlayComplete?: boolean;
+  color?: string;
 }
 
 const VoiceButton = ({
   text,
   onClick,
   isPlayComplete = false,
+  color = '#4F4A85',
 }: VoiceButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="flex w-[121px] h-[34px] items-center justify-center gap-2.5 px-[30px] py-1.5 bg-white rounded-[5px] font-normal text-[#5e72e4] text-xl tracking-[0] leading-5 whitespace-nowrap"
+      className="flex w-[121px] h-[34px] items-center justify-center gap-2.5 px-[30px] py-1.5 bg-white rounded-[5px] font-normal text-xl tracking-[0] leading-5 whitespace-nowrap"
+      style={{ color: color }}
     >
       {isPlayComplete ? 'Reply' : text || 'Voice'}
     </button>
