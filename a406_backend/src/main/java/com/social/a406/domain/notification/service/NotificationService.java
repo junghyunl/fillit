5c0eee494e65aa134bbd139a69cafa2c679b99b5
김지호 <nemo00528@gmail.com>
2025-01-30 17:14:23 +0900

    feat : 실시간 알림 SSE 적용

--- a406_backend/src/main/java/com/social/a406/domain/notification/service/NotificationService.java
+++ a406_backend/src/main/java/com/social/a406/domain/notification/service/NotificationService.java
@@ -10,8 +10,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 @Transactional
@@ -20,6 +24,34 @@ public class NotificationService {
 
     private final NotificationRepository notificationRepository;
     private final UserRepository userRepository;
+    private static final ConcurrentMap<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    private long TIME_OUT = 60 * 1000L; // 1분
+
+    public SseEmitter subscribe(String personalId){
+
+        SseEmitter emitter = new SseEmitter(TIME_OUT);
+        emitters.put(personalId, emitter);  // emitters 맵에 사용자와 emitter 연결 저장
+        // 연결 종료 시 emitter에서 사용자 제거
+        emitter.onCompletion(() -> emitters.remove(personalId));
+        // 타임아웃 발생 시 emitter에서 사용자 제거
+        emitter.onTimeout(() -> emitters.remove(personalId));
+        // 에러 발생 시 emitter에서 사용자 제거
+        emitter.onError((e) -> emitters.remove(personalId));
+        return emitter;
+    }
+
+    // 새로운 알림이 생기면 SSE로 사용자에게 전송
+    public void notifyUser(String username) {
+        SseEmitter emitter = emitters.get(username);
+        if (emitter != null) {
+            try {
+                emitter.send("new_notification");
+            } catch (IOException e) {
+                emitters.remove(username);
+            }
+        }
+    }
 
     public Notification createNotification(User receiver, User sender, NotificationType type, Long referenceId){
         if(receiver.equals(sender)){
@@ -35,15 +67,15 @@ public class NotificationService {
 
         notificationRepository.save(notification);
 
+        notifyUser(receiver.getPersonalId());
+
         return notification;
     }
 
     public List<Notification> getNotifications(UserDetails userDetails, Long cursorId, Pageable pageable) {
-        User user = userRepository.findByPersonalId(userDetails.getUsername()).orElse(null);
-
-        if(user == null){
-            throw new IllegalArgumentException("User not found");
-        }
+        User user = userRepository.findByPersonalId(userDetails.getUsername()).orElseThrow(
+                () -> new IllegalArgumentException("User Not found")
+        );
 
         List<Notification> notifications = notificationRepository.findAllByReceiverAndIsReadFalse(user, cursorId, pageable);
         return notifications;
