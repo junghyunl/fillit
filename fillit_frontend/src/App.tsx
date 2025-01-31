e426ae50325d33edc04c745248a635854121bc9a
이정현 <fnsk1256@naver.com>
2025-01-31 17:29:13 +0900

    feat: 게시글 상세 페이지 추가

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
+import ArticleListPage from '@/pages/ArticleListPage';
+import ArticleDetailPage from '@/pages/ArticleDetailPage';
 import Login from '@/pages/Login';
 import Message from '@/pages/Message';
 import Profile from '@/pages/Profile';
@@ -11,8 +12,10 @@ import NavBar from '@/components/common/NavBar';
 const App = () => {
   return (
     <>
+      <div className="bg-home-bg bg-fixed"></div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ArticleListPage />} />
+        <Route path="/article/:boardId" element={<ArticleDetailPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/message" element={<Message />} />
         <Route path="/profile" element={<Profile />} />
---
+++ fillit_frontend/src/pages/ArticleDetailPage.tsx
@@ -0,0 +1,17 @@
+import Header from '@/components/common/Header';
+import { useParams } from 'react-router-dom';
+
+const ArticleDetailPage = () => {
+  const { boardId } = useParams();
+
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Header left="back" right="menu" />
+      <div className="pt-[4.6rem]">
+        <h1>Article Detail Page #{boardId}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleDetailPage;
