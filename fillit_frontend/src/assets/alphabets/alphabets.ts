52bd156edbb92b20fb64e03cc8e2e9944bd9567c
이승희 <dearlsh1125@gmail.com>
2025-01-31 16:52:13 +0900

    feat: 알파벳 이미지 추가 및 alphabets.ts에서 import 관리

---
+++ fillit_frontend/src/assets/alphabets/alphabets.ts
@@ -0,0 +1,107 @@
+import ALow from '@/assets/alphabets/A-L.png';
+import AUp from '@/assets/alphabets/A-U.png';
+import BLow from '@/assets/alphabets/B-L.png';
+import BUp from '@/assets/alphabets/B-U.png';
+import CLow from '@/assets/alphabets/C-L.png';
+import CUp from '@/assets/alphabets/C-U.png';
+import DLow from '@/assets/alphabets/D-L.png';
+import DUp from '@/assets/alphabets/D-U.png';
+import ELow from '@/assets/alphabets/E-L.png';
+import EUp from '@/assets/alphabets/E-U.png';
+import FLow from '@/assets/alphabets/F-L.png';
+import FUp from '@/assets/alphabets/F-U.png';
+import GLow from '@/assets/alphabets/G-L.png';
+import GUp from '@/assets/alphabets/G-U.png';
+import HLow from '@/assets/alphabets/H-L.png';
+import HUp from '@/assets/alphabets/H-U.png';
+import ILow from '@/assets/alphabets/I-L.png';
+import IUp from '@/assets/alphabets/I-U.png';
+import JLow from '@/assets/alphabets/J-L.png';
+import JUp from '@/assets/alphabets/J-U.png';
+import KLow from '@/assets/alphabets/K-L.png';
+import KUp from '@/assets/alphabets/K-U.png';
+import LLow from '@/assets/alphabets/L-L.png';
+import LUp from '@/assets/alphabets/L-U.png';
+import MLow from '@/assets/alphabets/M-L.png';
+import MUp from '@/assets/alphabets/M-U.png';
+import NLow from '@/assets/alphabets/N-L.png';
+import NUp from '@/assets/alphabets/N-U.png';
+import OLow from '@/assets/alphabets/O-L.png';
+import OUp from '@/assets/alphabets/O-U.png';
+import PLow from '@/assets/alphabets/P-L.png';
+import PUp from '@/assets/alphabets/P-U.png';
+import QLow from '@/assets/alphabets/Q-L.png';
+import QUp from '@/assets/alphabets/Q-U.png';
+import RLow from '@/assets/alphabets/R-L.png';
+import RUp from '@/assets/alphabets/R-U.png';
+import SLow from '@/assets/alphabets/S-L.png';
+import SUp from '@/assets/alphabets/S-U.png';
+import TLow from '@/assets/alphabets/T-L.png';
+import TUp from '@/assets/alphabets/T-U.png';
+import ULow from '@/assets/alphabets/U-L.png';
+import UUp from '@/assets/alphabets/U-U.png';
+import VLow from '@/assets/alphabets/V-L.png';
+import VUp from '@/assets/alphabets/V-U.png';
+import WLow from '@/assets/alphabets/W-L.png';
+import WUp from '@/assets/alphabets/W-U.png';
+import XLow from '@/assets/alphabets/X-L.png';
+import XUp from '@/assets/alphabets/X-U.png';
+import YLow from '@/assets/alphabets/Y-L.png';
+import YUp from '@/assets/alphabets/Y-U.png';
+import ZLow from '@/assets/alphabets/Z-L.png';
+import ZUp from '@/assets/alphabets/Z-U.png';
+
+export {
+  ALow,
+  AUp,
+  BLow,
+  BUp,
+  CLow,
+  CUp,
+  DLow,
+  DUp,
+  ELow,
+  EUp,
+  FLow,
+  FUp,
+  GLow,
+  GUp,
+  HLow,
+  HUp,
+  ILow,
+  IUp,
+  JLow,
+  JUp,
+  KLow,
+  KUp,
+  LLow,
+  LUp,
+  MLow,
+  MUp,
+  NLow,
+  NUp,
+  OLow,
+  OUp,
+  PLow,
+  PUp,
+  QLow,
+  QUp,
+  RLow,
+  RUp,
+  SLow,
+  SUp,
+  TLow,
+  TUp,
+  ULow,
+  UUp,
+  VLow,
+  VUp,
+  WLow,
+  WUp,
+  XLow,
+  XUp,
+  YLow,
+  YUp,
+  ZLow,
+  ZUp,
+};
