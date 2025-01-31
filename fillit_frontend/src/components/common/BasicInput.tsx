a6141cc0c364f028daad2df4deb7c0b1eb259a54
이승희 <dearlsh1125@gmail.com>
2025-01-31 17:51:48 +0900

    design: outline 제거

--- fillit_frontend/src/components/common/BasicInput.tsx
+++ fillit_frontend/src/components/common/BasicInput.tsx
@@ -6,7 +6,7 @@ const BasicInput = ({ placeholder = 'Enter the text...' }: BasicInputProps) => {
   return (
     <input
       placeholder={placeholder}
-      className="bg-[#ffffff] px-3 py-1.5 w-[280px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2]"
+      className="bg-[#ffffff] px-3 py-1.5 w-[280px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none"
     />
   );
 };
