2a2c642f42a6713b10b8da944eca344db3ed3b51
김지호 <nemo00528@gmail.com>
2025-01-31 13:28:30 +0900

    feat : 댓글 대댓글 수 반환 추가

--- a406_backend/src/main/java/com/social/a406/domain/comment/repository/CommentRepository.java
+++ a406_backend/src/main/java/com/social/a406/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,5 @@
 package com.social.a406.domain.comment.repository;
 
-import com.social.a406.domain.board.entity.Board;
 import com.social.a406.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +8,4 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByBoard_IdOrderByCreatedAtAsc(Long boardId); //댓글 오래된순으로 조회
     long countByBoard_Id(Long boardId);
-
-    Long board(Board board);
 }
