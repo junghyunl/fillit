74e7ba4932cb5f0e1449b1282421fe76d726aa72
이정현 <fnsk1256@naver.com>
2025-01-31 17:31:05 +0900

    design: 알림페이지 흰배경으로 변경

--- fillit_frontend/src/pages/Notification.tsx
+++ fillit_frontend/src/pages/Notification.tsx
@@ -2,10 +2,10 @@ import Header from '@/components/common/Header';
 
 const Notification = () => {
   return (
-    <>
+    <div className="bg-white h-screen">
       <Header left="back" text="Notification" isTitle={true} />
       <h1>Notification</h1>
-    </>
+    </div>
   );
 };
 
