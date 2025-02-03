800d4a7af16131b208ff9e90305d402ee8f78c88
이승희 <dearlsh1125@gmail.com>
2025-02-03 13:18:16 +0900

    refactor: BasicButton 컴포넌트 동적 sizing 및 스타일 개선

--- fillit_frontend/src/components/common/Button/BasicButton.tsx
+++ fillit_frontend/src/components/common/Button/BasicButton.tsx
@@ -1,21 +1,30 @@
 interface BasicButtonProps {
   text?: string;
   onClick?: () => void;
+  width?: string;
+  height?: string;
 }
 
-const BasicButton = ({ text = 'Login', onClick }: BasicButtonProps) => {
+const BasicButton = ({
+  text = 'Login',
+  onClick,
+  width = '110px',
+  height = '40px',
+}: BasicButtonProps) => {
   return (
-    <>
-      <button className=" from-white to-transparent p-[4px] rounded-[16px]" onClick={onClick}>
-        <div
-          className=" group rounded-[5px] bg-gradient-to-b from-white shadow-[0_1px_3px_rgba(0,0,0,0.5)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995]"         
-        >
-          <div className="px-5 bg-gradient-to-b from-white to-white/80 rounded-[8px] py-1">
-            <span className="text-indigo-700 text-lg">{text}</span>
-          </div>
+    <button
+      className="from-white to-transparent p-[4px] rounded-[16px]"
+      onClick={onClick}
+      style={{ width, height }}
+    >
+      <div className="group rounded-[5px] bg-gradient-to-b from-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995] h-full">
+        <div className="px-5 bg-gradient-to-b from-white to-white/80 rounded-[8px] h-full flex items-center justify-center">
+          <span className="text-[#5e72e4] text-lg leading-none -translate-y-[1px]">
+            {text}
+          </span>
         </div>
-      </button>
-    </>
+      </div>
+    </button>
   );
 };
 
