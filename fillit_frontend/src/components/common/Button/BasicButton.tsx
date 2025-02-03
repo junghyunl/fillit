8d6bc100a136148f799b71fc78b554d3cfbff71f
이승희 <dearlsh1125@gmail.com>
2025-02-03 13:27:07 +0900

    refactor: 그림자 스타일 세부 조정

--- fillit_frontend/src/components/common/Button/BasicButton.tsx
+++ fillit_frontend/src/components/common/Button/BasicButton.tsx
@@ -17,7 +17,7 @@ const BasicButton = ({
       onClick={onClick}
       style={{ width, height }}
     >
-      <div className="group rounded-[5px] bg-gradient-to-b from-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995] h-full">
+      <div className="group rounded-[5px] bg-gradient-to-b from-white shadow-[0_4px_4px_rgba(0,0,0,0.25),_0_1px_10px_rgba(0,0,0,8%)]  active:shadow-[0_0px_1px_rgba(0,0,0,0.5)] active:scale-[0.995] h-full">
         <div className="px-5 bg-gradient-to-b from-white to-white/80 rounded-[8px] h-full flex items-center justify-center">
           <span className="text-[#5e72e4] text-lg leading-none -translate-y-[1px]">
             {text}
