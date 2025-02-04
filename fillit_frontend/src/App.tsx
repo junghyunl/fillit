6ed1bb80f4cdf05a8ab98583f021e9f843c2f25d
한병현 <gksqudgus777@naver.com>
2025-02-04 17:40:33 +0900

    feat: MessagePage UI 구현

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -4,6 +4,7 @@ import ArticleDetailPage from '@/pages/ArticleDetailPage';
 import FindPage from './pages/FindPage';
 import LoginPage from '@/pages/LoginPage';
 import MessageListPage from '@/pages/MessageListPage';
+import MessagePage from '@/pages/MessagePage';
 import ProfilePage from '@/pages/ProfilePage';
 import SearchPage from '@/pages/SearchPage';
 import SignUpPage from './pages/SignUpPage';
@@ -22,6 +23,7 @@ const App = () => {
         <Route path="/find" element={<FindPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/message" element={<MessageListPage />} />
+        <Route path="/message/:chatId" element={<MessagePage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/signup" element={<SignUpPage />} />
