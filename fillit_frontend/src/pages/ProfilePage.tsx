98bd3a6c107bb73e65a2f8ac04edc72b4300f294
이승희 <dearlsh1125@gmail.com>
2025-02-04 10:36:12 +0900

    refactor: 피드 배경 사진 추가, 레이아웃 리팩토링

--- fillit_frontend/src/pages/ProfilePage.tsx
+++ fillit_frontend/src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/common/Header';
 import ProfileImage from '@/components/common/ProfileImage';
-import { RippedProfile } from '@/assets/assets';
+import { ProfilePagePaper, RippedProfile } from '@/assets/assets';
 import { user } from '@/mocks/fixtures/user';
 import { useEffect, useState } from 'react';
 import { getPaperText } from '@/utils/getPaperText';
@@ -20,27 +20,27 @@ const ProfilePage = () => {
     <div className="container-header-nav">
       <Header left="home" right="menu" />
       <div className="w-full">
-        <div className="ml-20 -mt-5">
+        <div className="ml-16 -mt-7">
           {paperImage && (
-            <div className="w-[192px] h-[50px] mb-2 -ml-8 translate-y-28">
+            <div className="w-[12rem] h-[3.125rem] mb-2 -ml-8 translate-y-28">
               <img src={paperImage} alt="paper name" className="w-full" />
             </div>
           )}
           <ProfileImage src={user.profileImageUrl} size={101} />
-          <div className="w-[192px] h-[215px] -mt-[150px] -ml-[53px]">
+          <div className="w-[12rem] h-[13.438rem] -mt-[9.375rem] -ml-[53px]">
             <img src={RippedProfile} alt="ripped profile" />
           </div>
-          <div className="ml-[145px] -mt-[170px]">
+          <div className="ml-[9.1rem] -mt-[10.625rem]">
             <h2 className="text-2xl leading-tight">{user.name}</h2>
             <p className="text-gray-600 font-light text-sm italic -mt-1">
               @{user.personalId}
             </p>
             <div>
-              <div className="w-[135px] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-1">
+              <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-1">
                 <span className="text-sm font-light">follower</span>
                 <span className="ml-4 font-semibol">427</span>
               </div>
-              <div className="w-[135px] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
+              <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
                 <span className="text-sm font-light">following</span>
                 <span className="ml-2.5">427</span>
               </div>
@@ -48,11 +48,14 @@ const ProfilePage = () => {
           </div>
         </div>
         <div className="flex justify-center">
-          <div className="w-[351px] h-[51px] mt-7 bg-[#ffffff4c] rounded-[5px] flex items-center justify-center">
-            <p className="font-light text-black text-[13px] tracking-[0] leading-[15px] text-center px-8 text-[12px]">
+          <div className="w-[22rem] h-[3.2rem] mt-5 bg-[#ffffff4c] rounded-[5px] flex items-center justify-center">
+            <p className="font-light text-black tracking-[0] leading-[15px] text-center px-8 text-[12px]">
               {user.introduction}
             </p>
           </div>
+        </div>
+        <div className="flex justify-center scale-98 -mt-5">
+          <img src={ProfilePagePaper} alt="profile page paper" />
         </div>
       </div>
     </div>
