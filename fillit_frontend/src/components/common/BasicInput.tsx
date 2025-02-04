cd042e5ea1db9b079f8596c3e884e6db1a8e77e1
이승희 <dearlsh1125@gmail.com>
2025-02-04 17:33:49 +0900

    refactor: BasicInput 컴포넌트를 textarea로 변경 및 스타일 조정

--- fillit_frontend/src/components/common/BasicInput.tsx
+++ fillit_frontend/src/components/common/BasicInput.tsx
@@ -10,10 +10,10 @@ const BasicInput = ({
   height = 35,
 }: BasicInputProps) => {
   return (
-    <input
+    <textarea
       placeholder={placeholder}
       style={{ width: `${width}px`, height: `${height}px` }}
-      className="bg-[#ffffff] px-3 py-1.5 text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none"
+      className="bg-[#ffffff] px-3 py-1.5 text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none resize-none overflow-hidden"
     />
   );
 };
