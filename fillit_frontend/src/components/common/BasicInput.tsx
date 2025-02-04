bb190673068c419f87540896027e4243658e4619
이승희 <dearlsh1125@gmail.com>
2025-02-04 17:27:00 +0900

    refactor: BasicInput 컴포넌트 스타일링 방식 변경

--- fillit_frontend/src/components/common/BasicInput.tsx
+++ fillit_frontend/src/components/common/BasicInput.tsx
@@ -12,7 +12,8 @@ const BasicInput = ({
   return (
     <input
       placeholder={placeholder}
-      className={`bg-[#ffffff] px-3 py-1.5 w-[${width}px] h-[${height}px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none`}
+      style={{ width: `${width}px`, height: `${height}px` }}
+      className="bg-[#ffffff] px-3 py-1.5 text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none"
     />
   );
 };
