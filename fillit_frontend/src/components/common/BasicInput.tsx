4c9c18011613ab6f299e00f284002f1e26b2dcba
이승희 <dearlsh1125@gmail.com>
2025-02-04 17:23:33 +0900

    feat: BasicInput 컴포넌트에 width와 height 옵션 추가

--- fillit_frontend/src/components/common/BasicInput.tsx
+++ fillit_frontend/src/components/common/BasicInput.tsx
@@ -1,12 +1,18 @@
 interface BasicInputProps {
   placeholder?: string;
+  width?: number;
+  height?: number;
 }
 
-const BasicInput = ({ placeholder = 'Enter the text...' }: BasicInputProps) => {
+const BasicInput = ({
+  placeholder = 'Enter the text...',
+  width = 280,
+  height = 35,
+}: BasicInputProps) => {
   return (
     <input
       placeholder={placeholder}
-      className="bg-[#ffffff] px-3 py-1.5 w-[280px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none"
+      className={`bg-[#ffffff] px-3 py-1.5 w-[${width}px] h-[${height}px] text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none`}
     />
   );
 };
