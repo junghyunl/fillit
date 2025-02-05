c0117528ec5f2debf59b92de404edc6a58ea4c68
이승희 <dearlsh1125@gmail.com>
2025-02-05 17:54:58 +0900

    refactor: App.tsx imports using RouterProvider

--- fillit_frontend/src/App.tsx
+++ fillit_frontend/src/App.tsx
@@ -1,24 +1,5 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@/router';
-import { Routes, Route } from 'react-router-dom';
-import ArticleListPage from '@/pages/ArticleListPage';
-import ArticleDetailPage from '@/pages/ArticleDetailPage';
-import CommentDetailPage from '@/pages/CommentDetailPage';
-import FindPage from './pages/FindPage';
-import LoginPage from '@/pages/LoginPage';
-import MessageListPage from '@/pages/MessageListPage';
-import MessagePage from '@/pages/MessagePage';
-import ProfilePage from '@/pages/ProfilePage';
-import SearchPage from '@/pages/SearchPage';
-import SignUpPage from './pages/SignUpPage';
-import SocialPage from '@/pages/SocialSignUpPage';
-import VoicePage from '@/pages/VoicePage';
-import NotificationPage from '@/pages/NotificationPage';
-import NavBar from '@/components/common/NavBar/NavBar';
-import NewMessage from '@/pages/NewMassagePage';
-import { ProfileEditPage } from '@/pages/ProfileEditPage';
-import FollowerPage from './pages/FollowerPage';
-import FollowingPage from './pages/FollowingPage';
 
 const App = () => {
   return (
