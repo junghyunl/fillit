a79b22745da9d13095b9ca3eda8dff8ab0982811
이정현 <fnsk1256@naver.com>
2025-02-05 21:50:14 +0900

    feat: 대댓글 구현

---
+++ fillit_frontend/src/mocks/fixtures/replyList.ts
@@ -0,0 +1,51 @@
+import { Reply } from '@/types/reply';
+import ProfileImage from '@/mocks/images/profile-image.png';
+
+export const replyList: Reply[] = [
+  {
+    replyId: 0,
+    content: 'Today the wind was just perfect!',
+    personalId: 'Alex',
+    profileImageUrl: ProfileImage,
+    likeCount: 234,
+    createdAt: '2025-01-30T14:13:53.588Z',
+    updatedAt: '2025-01-29T19:13:53.588Z',
+  },
+  {
+    replyId: 1,
+    content:
+      'Today the wind was just perfect! Today the wind was just perfect!',
+    personalId: 'Alex',
+    profileImageUrl: ProfileImage,
+    likeCount: 234,
+    createdAt: '2025-01-30T14:13:53.588Z',
+    updatedAt: '2025-01-29T19:13:53.588Z',
+  },
+  {
+    replyId: 2,
+    content: 'Today the wind was just perfect!',
+    personalId: 'Alex',
+    profileImageUrl: ProfileImage,
+    likeCount: 234,
+    createdAt: '2025-01-30T14:13:53.588Z',
+    updatedAt: '2025-01-29T19:13:53.588Z',
+  },
+  {
+    replyId: 3,
+    content: 'Today the wind was just perfect!',
+    personalId: 'Alex',
+    profileImageUrl: ProfileImage,
+    likeCount: 234,
+    createdAt: '2025-01-30T14:13:53.588Z',
+    updatedAt: '2025-01-29T19:13:53.588Z',
+  },
+  {
+    replyId: 4,
+    content: 'Today the wind was just perfect!',
+    personalId: 'Alex',
+    profileImageUrl: ProfileImage,
+    likeCount: 234,
+    createdAt: '2025-01-30T14:13:53.588Z',
+    updatedAt: '2025-01-29T19:13:53.588Z',
+  },
+];
