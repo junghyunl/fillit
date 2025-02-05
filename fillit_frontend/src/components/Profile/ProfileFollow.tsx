b9f66fae8f915e79ec853f8734ea91d3f62cb070
이승희 <dearlsh1125@gmail.com>
2025-02-06 01:45:32 +0900

    refactor: ProfileFollow 컴포넌트 동적 데이터 및 라우트 처리

--- fillit_frontend/src/components/Profile/ProfileFollow.tsx
+++ fillit_frontend/src/components/Profile/ProfileFollow.tsx
@@ -1,28 +1,38 @@
 import { useNavigate } from 'react-router-dom';
 
-const ProfileFollow = () => {
+interface ProfileFollowProps {
+  followersCount: number;
+  followingCount: number;
+  personalId: string;
+}
+
+const ProfileFollow = ({
+  followersCount = 164,
+  followingCount = 795,
+  personalId,
+}: ProfileFollowProps) => {
   const navigate = useNavigate();
 
   const handleFollowerClick = () => {
-    navigate('/follower');
+    navigate(`/profile/${personalId}/follower`);
   };
 
   const handleFollowingClick = () => {
-    navigate('/following');
+    navigate(`/profile/${personalId}/following`);
   };
 
   return (
     <div>
       <button onClick={handleFollowerClick}>
         <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
-          <span className="ml-1 text-sm font-light">follower</span>
-          <span className="ml-4 font-semibol">427</span>
+          <span className="-ml-1 text-sm font-light">follower</span>
+          <span className="ml-4">{followersCount}</span>
         </div>
       </button>
       <button onClick={handleFollowingClick}>
         <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
-          <span className="ml-1 text-sm font-light">following</span>
-          <span className="ml-2.5">427</span>
+          <span className="-ml-1.5 text-sm font-light">following</span>
+          <span className="ml-2.5">{followingCount}</span>
         </div>
       </button>
     </div>
