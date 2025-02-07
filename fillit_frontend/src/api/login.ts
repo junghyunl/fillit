7cd13d4e313f23bb3422776a4529a05f544855b5
이정현 <fnsk1256@naver.com>
2025-02-07 16:42:40 +0900

    feat: 중복 이메일, 중복 personalId 체크 api 추가

--- fillit_frontend/src/api/login.ts
+++ fillit_frontend/src/api/login.ts
@@ -1,10 +1,26 @@
 import axiosInstance from '@/api/axiosInstance';
 
-// 일반 로그인
+/* 일반 로그인 */
 export const postLogin = async (email: string, password: string) => {
   const response = await axiosInstance.post('/api/users/login', {
     email,
     password,
+  });
+  return response.data;
+};
+
+/* 중복 이메일 체크 */
+export const postEmailCheck = async (input: string) => {
+  const response = await axiosInstance.post('/api/users/duplicate/email', {
+    input,
+  });
+  return response.data;
+};
+
+/* 중복 personalId 체크 */
+export const postPersonalIdCheck = async (input: string) => {
+  const response = await axiosInstance.post('/api/users/duplicate/nickname', {
+    input,
   });
   return response.data;
 };
