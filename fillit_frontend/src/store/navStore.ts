703b7d370609299799d94e0508ee05e3765e1bf7
한병현 <gksqudgus777@naver.com>
2025-02-07 17:53:35 +0900

    refactor: 상태 저장 및 재부팅시, 상태 초기화

--- fillit_frontend/src/store/navStore.ts
+++ fillit_frontend/src/store/navStore.ts
@@ -1,13 +1,22 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface NavStore {
-  activeNavItem: string | null;
+  activeNavItem: string;
   setActiveNavItem: (item: string) => void;
 }
 
-const useNavStore = create<NavStore>((set) => ({
-  activeNavItem: 'home',
-  setActiveNavItem: (item) => set({ activeNavItem: item }),
-}));
+const useNavStore = create<NavStore>()(
+  persist(
+    (set) => ({
+      activeNavItem: 'home',
+      setActiveNavItem: (item) => set({ activeNavItem: item }),
+    }),
+    {
+      name: 'nav-store',
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
 
 export default useNavStore;
