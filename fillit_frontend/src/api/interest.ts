48369c97099d85a4a07c8bf2cca3774d71ca503d
이정현 <fnsk1256@naver.com>
2025-02-07 15:12:45 +0900

    feat: 관심사 추가 api

---
+++ fillit_frontend/src/api/interest.ts
@@ -0,0 +1,13 @@
+import axiosInstance from '@/api/axiosInstance';
+
+// 관심사 추가
+export const postInterest = async (
+  personalId: string,
+  interestContents: string[]
+) => {
+  const response = await axiosInstance.post('/api/interests', {
+    personalId,
+    interestContents,
+  });
+  return response.data;
+};
