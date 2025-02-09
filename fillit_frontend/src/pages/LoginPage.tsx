74df136834df0be34fb31c15554ce53234d74a1b
이정현 <fnsk1256@naver.com>
2025-02-10 00:12:03 +0900

    feat: useStore 추가, 마이페이지 이동

--- fillit_frontend/src/pages/LoginPage.tsx
+++ fillit_frontend/src/pages/LoginPage.tsx
@@ -6,16 +6,19 @@ import BasicInput from '@/components/common/Input/BasicInput';
 import BasicButton from '@/components/common/Button/BasicButton';
 import SocialLogin from '@/components/common/SocialLogin';
 import { postLogin } from '@/api/login';
+import { useUserStore } from '@/store/useUserStore';
 
 const LoginPage = () => {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { setUser } = useUserStore();
 
   const handleLogin = async () => {
     try {
-      await postLogin(email, password);
+      const response = await postLogin(email, password);
+      setUser({ personalId: response.personalId });
       navigate('/');
     } catch (error) {
       alert('등록된 이메일이 아니거나 비밀번호가 다릅니다.');
