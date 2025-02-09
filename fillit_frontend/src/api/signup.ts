3e5dbe25594b15abb6e6bfc2eb7bf090176b7f9e
이정현 <fnsk1256@naver.com>
2025-02-09 15:15:15 +0900

    rename: 중복 체크 api 위치 변경 - login -> signup으로 변경

--- fillit_frontend/src/api/signup.ts
+++ fillit_frontend/src/api/signup.ts
@@ -21,3 +21,19 @@ export const postSignUp = async (signupForm: SignupForm) => {
   });
   return response.data;
 };
+
+/* 중복 이메일 체크 */
+export const postEmailCheck = async (input: string) => {
+  const response = await axiosInstance.post('/api/users/duplicate/email', {
+    input,
+  });
+  return response.data;
+};
+
+/* 중복 personalId 체크 */
+export const postPersonalIdCheck = async (input: string) => {
+  const response = await axiosInstance.post('/api/users/duplicate/nickname', {
+    input,
+  });
+  return response.data;
+};
