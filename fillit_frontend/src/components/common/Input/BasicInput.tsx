19ddb807d55487b06fe17867fc6b7228931c5057
이승희 <dearlsh1125@gmail.com>
2025-02-09 15:52:03 +0900

    refactor: 타입 안정성, 오류 처리 개선

--- fillit_frontend/src/components/common/Input/BasicInput.tsx
+++ fillit_frontend/src/components/common/Input/BasicInput.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 
-interface BasicInputProps {
+interface BasicInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
   width?: number;
   height?: number;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type?: string;
+  error?: string;
 }
 
-const BasicInput: React.FC<BasicInputProps> = ({
+const BasicInput = ({
   placeholder = 'Enter the text...',
   width = 280,
   height = 35,
   value,
   onChange,
   type = 'text',
-}) => {
+  error,
+  className,
+  ...props
+}: BasicInputProps) => {
   return (
-    <input
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      type={type}
-      style={{ width: `${width}px`, height: `${height}px` }}
-      className="bg-[#ffffff] px-3 py-1.5 text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] border-[#b5b4f2] outline-none resize-none overflow-hidden"
-    />
+    <div className="w-full flex justify-center">
+      <input
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        type={type}
+        style={{ width: `${width}px`, height: `${height}px` }}
+        className={`w-full p-3 text-[#757575] text-sm rounded-lg border-2 transition-colors duration-100 border-solid focus:border-[#596A95] ${
+          error ? 'border-red-500' : 'border-[#b5b4f2]'
+        } ${className}`}
+        {...props}
+      />
+      {error && <p className="text-red-500 text-xs mt-1">{error}</p>}
+    </div>
   );
 };
 
