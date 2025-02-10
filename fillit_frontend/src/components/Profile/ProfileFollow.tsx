acaf2af2cc9c044383b0d7b37e0ecc0a167841e6
이승희 <dearlsh1125@gmail.com>
2025-02-11 00:36:38 +0900

    refactor: 초기값 설정과 상태 관리 로직 추가

--- fillit_frontend/src/components/Profile/ProfileFollow.tsx
+++ fillit_frontend/src/components/Profile/ProfileFollow.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 interface ProfileFollowProps {
@@ -7,11 +8,19 @@ interface ProfileFollowProps {
 }
 
 const ProfileFollow = ({
-  followersCount,
-  followingCount,
+  followersCount: initialFollowersCount = 0,
+  followingCount: initialFollowingCount = 0,
   personalId,
 }: ProfileFollowProps) => {
+  const [followers, setFollowers] = useState(initialFollowersCount);
+  const [following, setFollowing] = useState(initialFollowingCount);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setFollowers(initialFollowersCount);
+    setFollowing(initialFollowingCount);
+  }, [initialFollowersCount, initialFollowingCount]);
 
   const handleFollowerClick = () => {
     navigate(`/profile/${personalId}/follower`);
@@ -26,13 +35,13 @@ const ProfileFollow = ({
       <button onClick={handleFollowerClick}>
         <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
           <span className="-ml-1 text-sm font-light">follower</span>
-          <span className="ml-4">{followersCount}</span>
+          <span className="ml-4">{followers}</span>
         </div>
       </button>
       <button onClick={handleFollowingClick}>
         <div className="w-[8.438rem] bg-[#ffffff1f] px-4 py-1.5 rounded-[999px] shadow-[0px_2px_4px_#00000040] mt-2">
           <span className="-ml-1.5 text-sm font-light">following</span>
-          <span className="ml-2.5">{followingCount}</span>
+          <span className="ml-2.5">{following}</span>
         </div>
       </button>
     </div>
