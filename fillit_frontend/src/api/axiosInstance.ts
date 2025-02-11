45119885bf060b12031b0b52e2b944708efd59e5
이정현 <fnsk1256@naver.com>
2025-02-12 03:40:46 +0900

    feat: access token만료 시 refresh token으로 재발급 추가

--- fillit_frontend/src/api/axiosInstance.ts
+++ fillit_frontend/src/api/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+import { getAccessToken } from '@/api/getAccessToken';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -12,18 +13,47 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const accessToken = localStorage.getItem('accessToken');
 
-  if (accessToken) {
-    config.headers['Authorization'] = `Bearer ${accessToken}`;
+  if (accessToken !== null) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
   }
+
+  console.log(
+    `[API request] ${config.method?.toUpperCase()} ${config.url}`,
+    config.data ? config.data : ''
+  );
 
   return config;
 });
 
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => {
+    console.log(
+      `[API response] ${response.status} ${response.config.url}`,
+      response.data
+    );
+
     return response;
   },
   async (error) => {
+    console.log('[API error] ', error);
+
+    const status = error?.response?.status;
+    const errorCode = error?.response?.data;
+
+    if (status === 401 && errorCode === 'Access token expired') {
+      try {
+        const { accessToken } = await getAccessToken();
+        if (accessToken) {
+          localStorage.setItem(
+            'accessToken',
+            accessToken.replace('Bearer ', '')
+          );
+        }
+      } catch (error) {
+        window.location.replace('/login');
+      }
+    }
+
     throw error;
   }
 );
