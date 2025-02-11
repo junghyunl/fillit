191533f6be30f7acac186662efc673457dd25985
이정현 <fnsk1256@naver.com>
2025-02-11 17:47:40 +0900

    feat: 게시글 리스트 api 연동, 무한스크롤 적용

--- fillit_frontend/src/components/Article/ArticleListContainer.tsx
+++ fillit_frontend/src/components/Article/ArticleListContainer.tsx
@@ -1,20 +1,45 @@
-import { articleList } from '@/mocks/fixtures/articleList';
 import ArticleCard from '@/components/Article/ArticleCard';
+import useGetFeed from '@/hooks/useGetFeed';
+import useIntersect from '@/hooks/useIntersect';
+import { FeedArticle } from '@/types/article';
 
 const ArticleListContainer = () => {
-  return (
-    <div className="flex flex-col items-center w-full overflow-x-hidden scrollbar-hide">
-      {articleList.map((article, index) => {
-        const position = index % 2 ? 'left' : 'right';
+  const { data, hasNextPage, isFetching, fetchNextPage } = useGetFeed(10);
 
-        return (
-          <ArticleCard
-            key={article.boardId}
-            article={article}
-            position={position}
-          />
-        );
-      })}
+  const pageEnd = useIntersect(async (entry, observer) => {
+    observer.unobserve(entry.target);
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    if (hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  });
+
+  return (
+    <div className="flex flex-col items-center pt-3 pb-20 w-full overflow-x-hidden">
+      {data?.pages?.length ? (
+        data.pages.map((page, pageIndex) => (
+          <div key={pageIndex}>
+            {page.posts.map((article: FeedArticle, index: number) => {
+              const position = index % 2 ? 'left' : 'right';
+
+              return (
+                <ArticleCard
+                  key={article.boardId}
+                  article={article}
+                  position={position}
+                />
+              );
+            })}
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
+
+      {hasNextPage && <div ref={pageEnd} style={{ height: 1 }} />}
+      {isFetching && <p>loading...</p>}
     </div>
   );
 };
---
+++ fillit_frontend/src/constants/queryKeys.ts
@@ -0,0 +1,4 @@
+export const QUERY_KEYS = {
+  FEED: 'feed',
+  ARTICLE: 'article',
+};
