827a2148303cb812a1f2916d377f327eccc4e59a
김지호 <nemo00528@gmail.com>
2025-02-12 16:23:55 +0900

    refactor : feed 페이지 네이션 수정

--- a406_backend/src/main/java/com/social/a406/domain/feed/service/FeedService.java
+++ a406_backend/src/main/java/com/social/a406/domain/feed/service/FeedService.java
@@ -60,7 +60,7 @@ public class FeedService {
         // 3. 친구 게시물 조회 – Feed 테이블에서 푸시된 데이터 (예: 80% 비율)
         int followLimit = (int) Math.ceil(limit * 0.8);
         PageRequest followPageable = PageRequest.of(0, followLimit, Sort.by("createdAt").descending());
-        List<Feed> feedEntries = feedRepository.findByUserIdAndcreatedAtBefore(userId, cursor, followPageable);
+        List<Feed> feedEntries = feedRepository.findByUserIdAndCreatedAtAfter(userId, cursor, followPageable);
         List<Board> friendBoards = feedEntries.stream()
                 .map(Feed::getBoard)
                 .collect(Collectors.toList());
