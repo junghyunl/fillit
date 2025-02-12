be3295c994de8e75791ab247becb9c61930aed33
hanjaegyeong <hjg000223@gmail.com>
2025-02-12 22:08:29 +0900

    feat: 레디스 환경변수 없으면 localhost 사용하도록 수정, 레디스 연결 로그

--- a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
+++ a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.social.a406.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@Slf4j
 public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(
-            @Value("${spring.data.redis.host:redis}") String redisHost,
+            @Value("${spring.data.redis.host:localhost}") String redisHost,
             @Value("${spring.data.redis.port:6379}") int redisPort) {
-        // 여기서 redisHost는 쿠버네티스 서비스 이름("redis")로 설정되어야 함.
+        log.info("Connecting to Redis at {}:{}", redisHost, redisPort);
         return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
