a9ba499f156f4df2cabbf161af841bc6cd6082a9
한재경 <hjg000223@naver.com>
2025-02-12 13:25:01 +0900

    feat: 테스트 컨텍스트에서 Redis 관련 자동 구성 비활성화 - 현재 테스트 시 Redis 연결 에러 - 테스트 시에 RedissonAutoConfiguration이 실행되지 않도록 함

--- a406_backend/src/test/java/com/social/a406/A406ApplicationTests.java
+++ a406_backend/src/test/java/com/social/a406/A406ApplicationTests.java
@@ -3,7 +3,9 @@ package com.social.a406;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = "spring.autoconfigure.exclude=org.redisson.spring.starter.RedissonAutoConfiguration"
+)
 class A406ApplicationTests {
 
 	@Test
