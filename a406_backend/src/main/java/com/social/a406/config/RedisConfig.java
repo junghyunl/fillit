403315071d61d3ffefd1107f62807927b2122d7c
hanjaegyeong <hjg000223@gmail.com>
2025-02-12 21:10:30 +0900

    feat: redis와 redisson 같이 사용할 수 있도록 RedisConfig 설정 - 현재 쿠버네티스 배포시 레디스 연결 오류

--- a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
+++ a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package com.social.a406.config;
 
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.redisson.config.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -10,17 +13,36 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    /**
+     * Spring Data Redis (Lettuce) 설정
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory("redis", 6379);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    /**
+     * Redisson 설정
+     */
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.setCodec(new StringCodec());
+        config.useSingleServer()
+                .setAddress("redis://redis:6379")
+                .setDatabase(0)
+                .setConnectionMinimumIdleSize(1)
+                .setConnectionPoolSize(10);
+
+        return org.redisson.Redisson.create(config);
     }
 }
