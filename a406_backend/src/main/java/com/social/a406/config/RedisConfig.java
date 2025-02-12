d053296026b4b2889c110926632d0d5b84a94c6b
hanjaegyeong <hjg000223@gmail.com>
2025-02-12 21:54:20 +0900

    feat: 젠킨스 테스트 원래대로 복구

--- a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
+++ a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
@@ -1,10 +1,8 @@
 package com.social.a406.config;
 
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,22 +11,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    @ConditionalOnMissingBean(RedisTemplate.class) // 테스트 환경에서는 빈 자동 제외
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(new LettuceConnectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        return template;
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
     }
 
     @Bean
-    @ConditionalOnMissingBean(RedissonClient.class) // 테스트 환경에서는 자동 제외
-    public RedissonClient redissonClient() {
-        Config config = new Config();
-        config.useSingleServer()
-                .setAddress("redis://redis:6379")
-                .setDatabase(0);
-        return org.redisson.Redisson.create(config);
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
     }
 }
