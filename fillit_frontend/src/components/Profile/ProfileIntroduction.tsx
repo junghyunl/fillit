b195887c23a9952b59ecdefd28cc97224053cae4
이정현 <fnsk1256@naver.com>
2025-02-13 02:17:19 +0900

    design: 프로필소개칸 가운데 정렬

--- fillit_frontend/src/components/Profile/ProfileIntroduction.tsx
+++ fillit_frontend/src/components/Profile/ProfileIntroduction.tsx
@@ -4,8 +4,8 @@ interface ProfileIntroductionProps {
 
 const ProfileIntroduction = ({ introduction }: ProfileIntroductionProps) => {
   return (
-    <div className="flex justify-center">
-      <div className="w-[22rem] h-[3.2rem] mt-5 bg-[#ffffff4c] rounded-[5px] flex items-center justify-center">
+    <div className="w-full px-5 flex justify-center">
+      <div className="w-full max-w-[22rem] h-[3.2rem] mt-5 bg-[#ffffff4c] rounded-[5px] flex items-center justify-center">
         <p className="font-light text-black tracking-[0] leading-[15px] text-center px-8 text-xs">
           {introduction}
         </p>
