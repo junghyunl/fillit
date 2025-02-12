1b404390330b8f84f9d4d1fc5ff5f0dbb34d651d
hanjaegyeong <hjg000223@gmail.com>
2025-02-12 22:02:59 +0900

    feat: 젠킨스 레디스 설정 환경변수 이용하도록 수정

--- a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
+++ a406_backend/src/main/java/com/social/a406/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.social.a406.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,14 +12,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.data.redis.host:redis}") String redisHost,
+            @Value("${spring.data.redis.port:6379}") int redisPort) {
+        // 여기서 redisHost는 쿠버네티스 서비스 이름("redis")로 설정되어야 함.
+        return new LettuceConnectionFactory(redisHost, redisPort);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
