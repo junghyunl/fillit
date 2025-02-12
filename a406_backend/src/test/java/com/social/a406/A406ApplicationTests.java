52837de14e659b2b17bfe7391726d99cc0d6be99
hanjaegyeong <hjg000223@gmail.com>
2025-02-12 21:20:43 +0900

    feat: 젠킨스 테스트 시 RedisConfig 테스트 배제

--- a406_backend/src/test/java/com/social/a406/A406ApplicationTests.java
+++ a406_backend/src/test/java/com/social/a406/A406ApplicationTests.java
@@ -1,11 +1,14 @@
 package com.social.a406;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-		properties = "spring.autoconfigure.exclude=org.redisson.spring.starter.RedissonAutoConfiguration"
-)
+@SpringBootTest
+@ImportAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration.class,
+		org.redisson.spring.starter.RedissonAutoConfiguration.class
+})
 class A406ApplicationTests {
 
 	@Test
