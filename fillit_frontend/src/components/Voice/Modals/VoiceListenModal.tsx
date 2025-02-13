dfb2542a6765262eb9f8df8a230ea1c77238a9f3
이승희 <dearlsh1125@gmail.com>
2025-02-14 02:11:05 +0900

    refactor: 보이스 재생후 답장 모달 열기

--- fillit_frontend/src/components/Voice/Modals/VoiceListenModal.tsx
+++ fillit_frontend/src/components/Voice/Modals/VoiceListenModal.tsx
@@ -1,10 +1,11 @@
 import { motion } from 'framer-motion';
 import { sound, playIcon2, soundWave, profileBubble } from '@/assets/assets';
 import VoiceBaseModal from './VoiceBaseModal';
-import { useNavigate } from 'react-router-dom';
 import VoiceButton from '@/components/common/Button/VoiceButton';
 import { useVoiceControl } from '@/hooks/useVoiceControl';
 import { Voice } from '@/types/voice';
+import { useState } from 'react';
+import ReplyRecordModal from './ReplyRecordModal';
 
 interface VoiceListenModalProps {
   voiceData: Voice | undefined;
@@ -17,7 +18,7 @@ const VoiceListenModal = ({
   isOpen,
   onClose,
 }: VoiceListenModalProps) => {
-  const navigate = useNavigate();
+  const [showReplyModal, setShowReplyModal] = useState(false);
   const { isPlaying, isFinished, currentDuration, handlePlay } =
     useVoiceControl({
       isModalOpen: isOpen,
@@ -29,10 +30,8 @@ const VoiceListenModal = ({
 
   const handleClick = () => {
     if (isFinished) {
-      console.log(
-        '[VoiceListenModal] 오디오 재생 완료됨, 답장 페이지로 이동합니다.'
-      );
-      navigate('/voice/reply', { state: { voiceData } });
+      console.log('[VoiceListenModal] 답장 모달 열기');
+      setShowReplyModal(true);
     } else {
       console.log('[VoiceListenModal] 오디오 재생 시작됨.');
       handlePlay();
@@ -40,62 +39,73 @@ const VoiceListenModal = ({
   };
 
   return (
-    <VoiceBaseModal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col items-center justify-center h-full gap-8 mt-12">
-        <div className="text-black text-4xl sm:text-5xl md:text-6xl font-medium">
-          {currentDuration}"
-        </div>
-        <div className="relative">
-          <motion.img
-            src={soundWave}
-            alt="sound wave"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: isPlaying ? 1 : 0 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.3 }}
-            className="absolute -bottom-2 w-full h-7"
-          />
-          <motion.img
-            src={voiceData.profileImageUrl || profileBubble}
-            alt="profile"
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.2 }}
-            className="w-[190px] h-[190px] rounded-full object-cover"
-          />
-        </div>
-        {isFinished ? (
-          <motion.div
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.4 }}
-          >
-            <VoiceButton
+    <>
+      <VoiceBaseModal isOpen={isOpen} onClose={onClose}>
+        <div className="flex flex-col items-center justify-center h-full gap-8 mt-12">
+          <div className="text-black text-4xl sm:text-5xl md:text-6xl font-medium">
+            {currentDuration}"
+          </div>
+          <div className="relative">
+            <motion.img
+              src={soundWave}
+              alt="sound wave"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: isPlaying ? 1 : 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.6, delay: 0.3 }}
+              className="absolute -bottom-2 w-full h-7"
+            />
+            <motion.img
+              src={voiceData.profileImageUrl || profileBubble}
+              alt="profile"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.6, delay: 0.2 }}
+              className="w-[190px] h-[190px] rounded-full object-cover"
+            />
+          </div>
+          {isFinished ? (
+            <motion.div
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+            >
+              <VoiceButton
+                onClick={handleClick}
+                text="Reply"
+                isPlayComplete={isFinished}
+              />
+            </motion.div>
+          ) : (
+            <motion.button
               onClick={handleClick}
-              text="Reply"
-              isPlayComplete={isFinished}
-            />
-          </motion.div>
-        ) : (
-          <motion.button
-            onClick={handleClick}
-            className="w-[46px] h-[47px]"
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.4 }}
-          >
-            <img
-              src={isPlaying ? sound : playIcon2}
-              alt={isPlaying ? 'sound' : 'play'}
-              className="w-full h-full"
-            />
-          </motion.button>
-        )}
-      </div>
-    </VoiceBaseModal>
+              className="w-[46px] h-[47px]"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 0.9, opacity: 0 }}
+              transition={{ duration: 0.6, delay: 0.4 }}
+            >
+              <img
+                src={isPlaying ? sound : playIcon2}
+                alt={isPlaying ? 'sound' : 'play'}
+                className="w-full h-full"
+              />
+            </motion.button>
+          )}
+        </div>
+      </VoiceBaseModal>
+
+      <ReplyRecordModal
+        isOpen={showReplyModal}
+        onClose={() => {
+          setShowReplyModal(false);
+          onClose();
+        }}
+        voiceData={voiceData}
+      />
+    </>
   );
 };
 
