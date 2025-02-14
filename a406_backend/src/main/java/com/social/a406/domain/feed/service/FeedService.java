dc14adc7e42f5869dbb449cbfcb5f1a72e041489
김현래 <gusto917@naver.com>
2025-02-14 14:07:20 +0900

    refactor : 좋아요 0개로 수정

--- a406_backend/src/main/java/com/social/a406/domain/feed/service/FeedService.java
+++ a406_backend/src/main/java/com/social/a406/domain/feed/service/FeedService.java
@@ -71,7 +71,7 @@ public class FeedService {
         for (Long interest : interests) {
             PageRequest recommendedPageable = PageRequest.of(0, recommendedLimit * 2, Sort.by("createdAt").descending());
             List<Board> fetchedRecommended = feedBoardRepository.findRecommendedBoards(interest,
-                    1,
+                    0,
                     LocalDateTime.now().minusDays(7),
                     recommendedPageable);
             recommendedBoards.addAll(fetchedRecommended);
