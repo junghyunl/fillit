cb35aef4b1e69d5f72c33c7800e8e41a314d594a
이승희 <dearlsh1125@gmail.com>
2025-02-14 16:51:42 +0900

    feat: 보이스 제출 로딩 중에 토스트 메세지 추가

--- fillit_frontend/src/components/Voice/Modals/ReplyRecordModal.tsx
+++ fillit_frontend/src/components/Voice/Modals/ReplyRecordModal.tsx
@@ -5,6 +5,8 @@ import VoiceButton from '@/components/common/Button/VoiceButton';
 import { useVoiceControl } from '@/hooks/useVoiceControl';
 import { postVoiceReply } from '@/api/voice';
 import { Voice } from '@/types/voice';
+import { useState } from 'react';
+import Toast from '@/components/common/Toast/Toast';
 
 interface ReplyRecordModalProps {
   isOpen: boolean;
@@ -17,6 +19,9 @@ const ReplyRecordModal = ({
   onClose,
   voiceData,
 }: ReplyRecordModalProps) => {
+  const [showToast, setShowToast] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const {
     isPlaying: isRecording,
     isFinished: isRecordingComplete,
@@ -48,78 +53,89 @@ const ReplyRecordModal = ({
   const handleSubmit = async () => {
     if (recordedFile) {
       try {
+        setIsLoading(true);
+        setShowToast(true);
         await postVoiceReply(recordedFile, voiceData.voiceId);
         console.log('[ReplyRecordModal] 답장 업로드 성공.');
-        onClose();
       } catch (error) {
         console.error('[ReplyRecordModal] 답장 업로드 실패:', error);
+      } finally {
+        setShowToast(false);
+        setIsLoading(false);
+        onClose();
       }
     }
   };
 
   return (
-    <VoiceBaseModal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col items-center justify-center h-full gap-8 mt-12">
-        <div className="text-black text-4xl sm:text-5xl md:text-6xl font-medium">
-          {currentDuration}"
-        </div>
+    <>
+      <VoiceBaseModal isOpen={isOpen} onClose={onClose}>
+        <div className="flex flex-col items-center justify-center h-full gap-8 mt-12">
+          <div className="text-black text-4xl sm:text-5xl md:text-6xl font-medium">
+            {currentDuration}"
+          </div>
 
-        <div className="relative">
-          {!isRecording && !isRecordingComplete ? (
-            <motion.img
-              src={micBig}
-              alt="microphone"
+          <div className="relative">
+            {!isRecording && !isRecordingComplete ? (
+              <motion.img
+                src={micBig}
+                alt="microphone"
+                initial={{ scale: 0.9, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.9, opacity: 0 }}
+                transition={{ duration: 0.6, delay: 0.2 }}
+                className="w-[120px] h-[160px] cursor-pointer"
+                onClick={handleMicClick}
+              />
+            ) : (
+              <motion.img
+                src={isRecording ? soundWave : micBig}
+                alt={isRecording ? 'Sound Wave' : 'Microphone'}
+                initial={{ scale: 0.9, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.9, opacity: 0 }}
+                transition={{ duration: 0.6, delay: 0.2 }}
+                className="w-[120px] h-[160px]"
+              />
+            )}
+          </div>
+
+          {isRecording ? (
+            <VoiceButton onClick={handleStop} text="Stop" />
+          ) : isRecordingComplete ? (
+            <motion.div
               initial={{ scale: 0.9, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.9, opacity: 0 }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-              className="w-[120px] h-[160px] cursor-pointer"
-              onClick={handleMicClick}
-            />
+              transition={{ duration: 0.6, delay: 0.4 }}
+            >
+              <div className="flex gap-[85px]">
+                <VoiceButton
+                  onClick={handleReRecord}
+                  text="Re-Record"
+                  color="#D68DE1"
+                />
+                <VoiceButton onClick={handleSubmit} text="Submit" />
+              </div>
+            </motion.div>
           ) : (
             <motion.img
-              src={isRecording ? soundWave : micBig}
-              alt={isRecording ? 'Sound Wave' : 'Microphone'}
-              initial={{ scale: 0.9, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.9, opacity: 0 }}
-              transition={{ duration: 0.6, delay: 0.2 }}
-              className="w-[120px] h-[160px]"
+              src={soundWave}
+              alt="sound wave"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: isRecording ? 1 : 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.6, delay: 0.3 }}
+              className="w-[121px] h-7"
             />
           )}
         </div>
-
-        {isRecording ? (
-          <VoiceButton onClick={handleStop} text="Stop" />
-        ) : isRecordingComplete ? (
-          <motion.div
-            initial={{ scale: 0.9, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ scale: 0.9, opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.4 }}
-          >
-            <div className="flex gap-[85px]">
-              <VoiceButton
-                onClick={handleReRecord}
-                text="Re-Record"
-                color="#D68DE1"
-              />
-              <VoiceButton onClick={handleSubmit} text="Submit" />
-            </div>
-          </motion.div>
-        ) : (
-          <motion.img
-            src={soundWave}
-            alt="sound wave"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: isRecording ? 1 : 0 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.6, delay: 0.3 }}
-            className="w-[121px] h-7"
-          />
-        )}
-      </div>
-    </VoiceBaseModal>
+      </VoiceBaseModal>
+      <Toast
+        message="Just sent the voice bubble, fam! 🫧💬"
+        isVisible={showToast && isLoading}
+      />
+    </>
   );
 };
 
