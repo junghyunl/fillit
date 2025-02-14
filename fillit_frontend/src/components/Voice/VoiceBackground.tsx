b339369242da325feea785edc7ea4ed1c70391d2
이승희 <dearlsh1125@gmail.com>
2025-02-14 13:00:21 +0900

    design: 보이스버블 배경 크기 키움

--- fillit_frontend/src/components/Voice/VoiceBackground.tsx
+++ fillit_frontend/src/components/Voice/VoiceBackground.tsx
@@ -6,17 +6,17 @@ const VoiceBackground = () => {
       <img
         src={bubble1}
         alt="bubble1"
-        className="absolute w-[317px] h-[325px] top-0 right-0 object-cover"
+        className="absolute w-[317px] h-[325px] top-0 right-0 object-cover scale-110  "
       />
       <img
         src={bubble2}
         alt="bubble2"
-        className="absolute w-[323px] h-[497px] top-[127px] left-0 object-cover"
+        className="absolute w-[323px] h-[497px] top-[127px] left-0 object-cover scale-110"
       />
       <img
         src={bubble3}
         alt="bubble3"
-        className="absolute w-[391px] h-[587px] right-0 bottom-0"
+        className="absolute w-[391px] h-[587px] right-0 bottom-0 scale-110"
       />
     </div>
   );
