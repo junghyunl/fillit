dc24e0e5102e2db3eeee7b7bef6d8a231a96fd56
이승희 <dearlsh1125@gmail.com>
2025-02-17 10:05:06 +0900

    refactor: 에러 타입 지정

--- fillit_frontend/src/pages/FindPasswordPage.tsx
+++ fillit_frontend/src/pages/FindPasswordPage.tsx
@@ -29,11 +29,15 @@ const FindPasswordPage = () => {
       await postEmailCode(email, personalId);
       setStep(1);
       setError('');
-    } catch (error: any) {
-      if (error.message.includes('이메일 전송')) {
+    } catch (error: Error | unknown) {
+      if (error instanceof Error && error.message.includes('이메일 전송')) {
         setError('이메일 전송에 실패했습니다. 잠시 후 다시 시도해주세요.');
       } else {
-        setError(error.message || '알 수 없는 오류가 발생했습니다.');
+        setError(
+          error instanceof Error
+            ? error.message
+            : '알 수 없는 오류가 발생했습니다.'
+        );
       }
     }
   };
