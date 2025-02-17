4282d454f1e71383b3e623d5c7a4e684e97118b8
한병현 <gksqudgus777@naver.com>
2025-02-17 13:58:24 +0900

    refactor: 페이지 이동 리팩토링

--- fillit_frontend/src/components/common/Button/BackButton.tsx
+++ fillit_frontend/src/components/common/Button/BackButton.tsx
@@ -1,11 +1,16 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { BackIcon } from '@/assets/assets';
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   const handleGoBack = () => {
-    navigate(-1);
+    if (location.state && location.state.fromEdit) {
+      navigate('/', { replace: true });
+    } else {
+      navigate(-1);
+    }
   };
 
   return (
