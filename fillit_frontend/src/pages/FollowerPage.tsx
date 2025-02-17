b27e777ec71ff51b1edaf1c3e0797751f09e5aad
이승희 <dearlsh1125@gmail.com>
2025-02-17 18:26:58 +0900

    refactor: query 사용하여 프로필 데이터 가져오기

--- fillit_frontend/src/pages/FollowerPage.tsx
+++ fillit_frontend/src/pages/FollowerPage.tsx
@@ -1,31 +1,17 @@
-import { getUserProfile } from '@/api/user';
 import Header from '@/components/common/Header/Header';
 import UserList from '@/components/Profile/UserList';
-import { useEffect, useState } from 'react';
+import { useGetProfile } from '@/hooks/query/useGetProfile';
 import { useParams } from 'react-router-dom';
 
 const FollowerPage = () => {
   const { personalId } = useParams();
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    const fetchUserName = async () => {
-      if (!personalId) return;
-      try {
-        const userData = await getUserProfile(personalId);
-        setUserName(userData.name);
-      } catch (error) {
-        console.error('사용자 정보 조회 실패', error);
-      }
-    };
-    fetchUserName();
-  }, [personalId]);
+  const { data: userData, isLoading } = useGetProfile(personalId ?? '');
 
   return (
     <div className="container-header-nav overflow-hidden">
       <Header
         left="back"
-        text={`${userName}'s Followers`}
+        text={isLoading ? 'Loading...' : `${userData?.name}'s hype house`}
         right="notification"
       />
       <UserList type="followers" personalId={personalId} />
