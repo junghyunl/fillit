f05008e857b9239352d1dc8e9b313f4eb9eccb2e
한재경 <hjg000223@naver.com>
2025-02-18 16:34:19 +0900

    feat: 스케줄러 스레드풀 10으로 증가

--- a406_backend/src/main/java/com/social/a406/config/SchedulerConfig.java
+++ a406_backend/src/main/java/com/social/a406/config/SchedulerConfig.java
@@ -12,7 +12,7 @@ public class SchedulerConfig {
     @Bean
     public ThreadPoolTaskScheduler taskScheduler() {
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
-        scheduler.setPoolSize(5); // 5개의 스레드를 허용
+        scheduler.setPoolSize(10); // 10개의 스레드를 허용
         scheduler.setThreadNamePrefix("TaskScheduler-");
         scheduler.initialize();
         return scheduler;
