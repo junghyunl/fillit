7a2c87fbf7eb4acd4fae854f95030cca258cc9d4
이정현 <fnsk1256@naver.com>
2025-02-19 03:41:57 +0900

    fix: 경고창 위치를 중앙으로 변경

--- fillit_frontend/src/components/common/Toast/Toast.tsx
+++ fillit_frontend/src/components/common/Toast/Toast.tsx
@@ -7,14 +7,14 @@ const Toast = () => {
 
   return (
     <div
-      className={`fixed top-2/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[200] ${
+      className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[200] ${
         show ? 'opacity-100 translate-x-0' : 'opacity-0 translate-x-full'
       }`}
       style={{
         transition: 'opacity 0.3s, transform 0.3s ease-in-out',
       }}
     >
-      <div className="bg-white/90 backdrop-blur-sm px-6 py-3 rounded-full shadow-lg">
+      <div className="bg-white/90 backdrop-blur-sm px-4 ml-8 py-3 rounded-full shadow-xl">
         <p className="text-[#4F4A85] text-sm font-medium">{toast}</p>
       </div>
     </div>
