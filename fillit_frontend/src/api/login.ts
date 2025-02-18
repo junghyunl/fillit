e3c0e55bce8959f6537091e5c99350d058c3a825
이승희 <dearlsh1125@gmail.com>
2025-02-18 10:59:35 +0900

    fix: email로 재변경

--- fillit_frontend/src/api/login.ts
+++ fillit_frontend/src/api/login.ts
@@ -1,12 +1,9 @@
 import axiosInstance from '@/api/axiosInstance';
 
 /* 일반 로그인 */
-export const postLogin = async (
-  emailOrPersonalId: string,
-  password: string
-) => {
+export const postLogin = async (email: string, password: string) => {
   const response = await axiosInstance.post('/api/users/login', {
-    emailOrPersonalId,
+    email,
     password,
   });
 
