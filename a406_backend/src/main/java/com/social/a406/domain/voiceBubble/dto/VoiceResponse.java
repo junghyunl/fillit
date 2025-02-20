c4a13b4d83b022e78753ff9709d4c9300b42f0e0
김지호 <nemo00528@gmail.com>
2025-02-20 17:57:09 +0900

    refactor : voice list 반환 name 추가

--- a406_backend/src/main/java/com/social/a406/domain/voiceBubble/dto/VoiceResponse.java
+++ a406_backend/src/main/java/com/social/a406/domain/voiceBubble/dto/VoiceResponse.java
@@ -2,6 +2,7 @@ package com.social.a406.domain.voiceBubble.dto;
 
 public record VoiceResponse(
     Long voiceId,
+    String name,
     String personalId,
     String profileImageUrl,
     String audioUrl
